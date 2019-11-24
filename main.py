14bcb969c4d88b00be0078368067bb07b7fa1324
Kaela Nichols <kae.nichols28@gmail.com>
2019-11-24 13:38:43 -0600

    added caesar.py, TODOs complete function in main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask
-
+from flask import Flask, request
+from caesar import rotate_string
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -26,12 +26,27 @@ form = """
         </style>
     </head>
     <body>
-      < input stuff here...
+      <form methods="POST">
+      <label></label>
+        Rotate By: <input type="text" name="rot" value="0"/>
+        <textarea name="text">
+        </textarea>
+        <input type="submit" value="Submit Query">
+        
+      </form>
     </body>
 </html>
  """  
 @app.route("/")
 def index():
+    return form
+
+@app.route("/", methods=['POST'])    
+def encrypt(rot, text):
+    rotate = int(rot)
+    inputText = text
+    output = rotate_string(inputText, rotate)
+    return output 
 
 
 app.run()
