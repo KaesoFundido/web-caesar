32249d74829eec5a739fe6f1974d97a72e13bb89
Kaela Nichols <kae.nichols28@gmail.com>
2019-11-24 14:45:12 -0600

    rotated text return in form complete

--- main.py
+++ main.py
@@ -10,26 +10,26 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
-      <form methods="POST">
+      <form method="POST">
       <label></label>
         Rotate By: <input type="text" name="rot" value="0"/>
-        <textarea name="text">
+        <textarea name="text"> {0}
         </textarea>
         <input type="submit" value="Submit Query">
         
@@ -39,14 +39,14 @@ form = """
  """  
 @app.route("/")
 def index():
-    return form
+    return form.format('')
 
 @app.route("/", methods=['POST'])    
-def encrypt(rot, text):
-    rotate = int(rot)
-    inputText = text
+def encrypt():
+    rotate = int(request.form['rot'])
+    inputText = request.form['text']
     output = rotate_string(inputText, rotate)
-    return output 
+    return form.format(output) 
 
 
 app.run()
